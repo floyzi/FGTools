e2fd81855a179cb3e2fba674873817afbfa55e6c
Floyzi <72631687+floyzi@users.noreply.github.com>
2026-08-29 02:05:53 +0300

    feat(cosmetics): add search bar animations

--- Src/FGTools/Internal/Behaviours/CosmeticSearchBar.cs
+++ Src/FGTools/Internal/Behaviours/CosmeticSearchBar.cs
@@ -8,23 +8,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using UnityEngine;
+using UnityEngine.UIElements;
 using static FGTools.Services.CosmeticsService;
 
 namespace FGTools.Internal.Behaviours
 {
     internal class CosmeticSearchBar : ToolsBehaviour
     {
-        CosmeticsService Service;
-        Coroutine Delay;
-        UICustomTMP_InputField InputField;
-        DebugDisplayService DebugService;
+        CosmeticsService _service;
+        Coroutine _delay;
+        UICustomTMP_InputField _inputField;
+        DebugDisplayService _debugService;
+        Vector2 _initPos;
+        RectTransform _rectTransform;
         void Awake()
         {
-            Service = FGTServiceManager.GetService<CosmeticsService>();
-            DebugService = FGTServiceManager.GetService<DebugDisplayService>();
+            _rectTransform = GetComponent<RectTransform>();
+            _initPos = _rectTransform.anchoredPosition;
 
-            InputField = GetComponentInChildren<UICustomTMP_InputField>();
-            var tween = InputField.GetComponent<TweenOnTMP_InputField>();
+            _service = FGTServiceManager.GetService<CosmeticsService>();
+            _debugService = FGTServiceManager.GetService<DebugDisplayService>();
+
+            _inputField = GetComponentInChildren<UICustomTMP_InputField>();
+            var tween = _inputField.GetComponent<TweenOnTMP_InputField>();
+
+            Reset();
 
             //too lazy to figure out why it does not work properly natively (
             //this works just fine so why even bother
@@ -40,47 +48,47 @@ namespace FGTools.Internal.Behaviours
                     AudioManager.PlayOneShot(tween._onSelectAudio);
                     tween._toggleOn.DOPunchScale(tween._toSize, tween._bounceTime, tween._bounceNumber, tween._bouncePower);
 
-                    InputField.Select();
-                    InputField.ActivateInputField();
-                    InputField.caretWidth = 1;
-                    DebugService.CanTriggerDebug = false;
+                    _inputField.Select();
+                    _inputField.ActivateInputField();
+                    _inputField.caretWidth = 1;
+                    _debugService.CanTriggerDebug = false;
 
-                    Service.SearchStart();
+                    _service.SearchStart();
                 }
                 else
                 {
-                    InputField.DeactivateInputField();
-                    InputField.caretWidth = 0;
-                    DebugService.CanTriggerDebug = true;
+                    _inputField.DeactivateInputField();
+                    _inputField.caretWidth = 0;
+                    _debugService.CanTriggerDebug = true;
 
-                    Service.SearchEnd(false);
+                    _service.SearchEnd(false);
                 }
             });
 
-            InputField.onEndEdit.AddListener(new Action<string>((s) =>
+            _inputField.onEndEdit.AddListener(new Action<string>((s) =>
             {
-                DebugService.CanTriggerDebug = true;
-                Service.SearchEnd(false);
+                _debugService.CanTriggerDebug = true;
+                _service.SearchEnd(false);
             }));
 
-            InputField.onSelect.AddListener(new Action<string>((s) =>
+            _inputField.onSelect.AddListener(new Action<string>((s) =>
             {
-                DebugService.CanTriggerDebug = false;
-                Service.SearchStart();
+                _debugService.CanTriggerDebug = false;
+                _service.SearchStart();
             }));
 
-            InputField.onValueChanged.AddListener(new Action<string>((s) => 
+            _inputField.onValueChanged.AddListener(new Action<string>((s) => 
             {
-                if (Delay != null) CoroutineRunner.Instance.StopCoroutine(Delay);
-                Delay = CoroutineRunner.Instance.StartCoroutine(DelayedSearch(s).WrapToIl2Cpp());
+                if (_delay != null) CoroutineRunner.Instance.StopCoroutine(_delay);
+                _delay = CoroutineRunner.Instance.StartCoroutine(DelayedSearch(s).WrapToIl2Cpp());
             }));
         }
 
         IEnumerator DelayedSearch(string s)
         {
             yield return new WaitForSeconds(0.35f);
-            Service.Search(s, Service.GetSection(), RequestType.Locker, Config.Config.AllCosmetics.Value);
-            Delay = null;
+            _service.Search(s, _service.GetSection(), RequestType.Locker, Config.Config.AllCosmetics.Value);
+            _delay = null;
         }
 
         void OnDisable()
@@ -91,12 +99,15 @@ namespace FGTools.Internal.Behaviours
         void OnEnable()
         {
             Reset();
+            _rectTransform.anchoredPosition = _initPos + Vector2.up * 80f;
+            _rectTransform.DOAnchorPos(new(_rectTransform.anchoredPosition.x, _initPos.y), 0.2f);
         }
 
         void Reset()
         {
-            DebugService.CanTriggerDebug = true;
-            InputField.text = "";
+            _debugService.CanTriggerDebug = true;
+            _inputField.text = "";
+            _rectTransform.DOKill();
         }
     }
 }
