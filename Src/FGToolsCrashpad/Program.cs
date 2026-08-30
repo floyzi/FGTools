b84af7d7416fd194022c766f299728d6cf628b90
Floyzi <72631687+floyzi@users.noreply.github.com>
2026-08-31 02:49:22 +0300

    feat(crashpad): implement report logic

--- Src/FGToolsCrashpad/Program.cs
+++ Src/FGToolsCrashpad/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.IO.Compression;
 using System.Runtime.InteropServices;
 
 namespace FGToolsCrashpad
@@ -29,10 +30,38 @@ namespace FGToolsCrashpad
             var errorLog = Path.Combine(bepinPath, "ErrorLog.log");
             if (!File.Exists(errorLog) || string.IsNullOrWhiteSpace(File.ReadAllText(errorLog))) return;
 
-            var box = MessageBox(IntPtr.Zero, "Looks like the game crashed! It is highly recommended for you to send a proper bug report in the FGTools Discord Server, it will help me to fix the issue that caused this and make sure this never happens again\n\nWould you like to open crashes folder?", "UH OH", 0x00000004 | 0x00000010);
+            var localLow = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "..", "LocalLow");
+            var unityLog = Path.Combine(localLow, "Mediatonic", "FallGuys_client", "Player.log");
+            var logOutput = Path.Combine(bepinPath, "LogOutput.log");
+            var pluginsPath = Path.Combine(bepinPath, "plugins");
+            var fgtPath = Path.Combine(pluginsPath, "FGTools");
+            var reportsPath = Path.Combine(fgtPath, "Reports", "Crashes");
+
+            if (!Directory.Exists(pluginsPath) || !Directory.Exists(fgtPath)) return;
+
+            if (!Directory.Exists(reportsPath)) Directory.CreateDirectory(reportsPath);
+
+            var reportName = $"CrashReport_{DateTime.Now:HHMMssFF}";
+            var report = Path.Combine(reportsPath, reportName);
+            if (Directory.Exists(report)) Directory.Delete(report, true);
+
+            Directory.CreateDirectory(report);
+
+            File.Copy(logOutput, Path.Combine(report, "LogOutput.log"));
+            File.Copy(errorLog, Path.Combine(report, "ErrorLog.log"));
+            File.Copy(unityLog, Path.Combine(report, "Player.log"));
+
+            ZipFile.CreateFromDirectory(report, Path.Combine(reportsPath, $"{reportName}.zip"));
+            Directory.Delete(report, true);
+
+            var box = MessageBox(IntPtr.Zero, "Сongratulations! Your game just crashed!\n" +
+                $"A report with name \"{reportName}\" was created\n\n" +
+                $"It is highly recommended for you to send this report in the FGTools Discord Server, it will help me fix the issue that caused this crash and make sure it never happens again (can't promise that though)\n\n" +
+                $"Would you like to open reports folder? This report is saved as \"{reportName}.zip\"", "Welp...", 0x00000004 | 0x00000010);
+            
             if (box == 6)
             {
-                
+                Process.Start("explorer.exe", @reportsPath);
             }
         }
     }
